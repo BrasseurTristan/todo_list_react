310b5d9c53c13bb17b3924ba0e078d876545343d
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-18 11:44:03 +0100

    Ajout de la création de la tâche dans une liste de tâches

--- src/components/taskContainer.jsx
+++ src/components/taskContainer.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Footer } from './footer/footer'
 import { Header } from './header/header'
 import { TaskInput } from './taskInput/taskInput'
@@ -5,10 +6,23 @@ import { TaskList } from './taskList/taskList'
 // Ce composant est utilisé pour afficher l'intégralité de la liste de tâches
 
 export const TaskContainer = () => {
+
+    const [tasksList, setTasksList] = useState([]);
+
+    const addTask = (title) => {
+        const newTask = {
+            id: tasksList.length + 1,
+            title: title,
+            completed: false,
+        }
+        //On met à jour l'état de Tasklist avec tout ce qu'elle contient déjà + une nouvelle entrée qui est newTask
+        setTasksList([...tasksList, newTask])
+    }
+
     return (
         <main>
             <Header />
-            <TaskInput />
+            <TaskInput addTask={addTask} />
             <TaskList />
             <Footer />
         </main>
--- src/components/taskInput/taskInput.jsx
+++ src/components/taskInput/taskInput.jsx
@@ -1,15 +1,36 @@
 // Ce composant sera utilisé pour afficher le champ de saisie de tâche.
 
+import { useState } from 'react';
 import styles from './TaskInput.module.css'
-export const TaskInput = () => {
+import { TaskList } from '../taskList/taskList';
+
+export const TaskInput = ({ addTask }) => {
+
+    const [taskTitle, setTaskTitle] = useState("");
+
+    const handleInputChange = (e) => {
+        // On insère chaque changement capturer dans taskTitle grâce à setTaskTitle
+        setTaskTitle(e.target.value);
+    }
+    const handleAddTask = (e) => {
+        // on empêche le rechargement de la page 
+        e.preventDefault();
+        if (taskTitle.trim()) {
+            // on utilise la fonction qu'on a importer de taskContainer pour y insérer le titre
+            addTask(taskTitle);
+            // On vide setTaskTitle
+            setTaskTitle("");
+        }
+    }
     return (
         <div className={`box ${styles.element}`}>
             <h2 className={styles.title}> Ajoute ta nouvelle tâche</h2>
-            <form className={styles.container}>
-                <input className={styles.input} type="text" placeholder='Indiquez un titre de tâche explicite.' />
+            {/* On éxécute la fonction handleAddTask lors de la soumission du formulaire  */}
+            <form className={styles.container} onSubmit={handleAddTask}>
+                {/* Dans l'input la fonction handleInputChange capture chaques changements */}
+                <input className={styles.input} type="text" placeholder='Indiquez un titre de tâche explicite.' onChange={handleInputChange} value={taskTitle} />
                 <button className="button-primary" type='submit'>Ajouter</button>
             </form>
-
         </div>
     );
 };
