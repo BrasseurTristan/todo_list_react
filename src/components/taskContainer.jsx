fa1c95f6c8a98114c6f6013af240cff187dcb912
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-18 09:25:14 +0100

    Création du header

---
+++ src/components/taskContainer.jsx
@@ -0,0 +1,10 @@
+import { Header } from './header/header'
+// Ce composant est utilisé pour afficher l'intégralité de la liste de tâches
+
+export const TaskContainer = () => {
+    return (
+        <main>
+            <Header />
+        </main>
+    )
+}
