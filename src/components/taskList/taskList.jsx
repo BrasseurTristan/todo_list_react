0efbd8c789bccfc179788dcbbae5eaf4dcbc86eb
BrasseurTristan <brasseur.tristan@gmail.com>
2025-02-18 10:28:31 +0100

    Création de la liste des tâches

---
+++ src/components/taskList/taskList.jsx
@@ -0,0 +1,17 @@
+// Ce composant est utilisé pour afficher la liste des tâches.
+
+import styles from "./taskList.module.css";
+
+export const TaskList = () => {
+
+    return (
+
+        <div className="box">
+            <h2 className={styles.title}> Il te reste encore X tâches à accomplir !</h2>
+            <ul className={styles.container}>
+
+            </ul>
+        </div>
+
+    );
+}
